faf4ab0350a9d9d9e08c9041d06e40febb03a020
Jerry(gwangjin) Jeong <gwangjin.jeong@dankook.ac.kr>
2021-10-05 21:08:12 +0900

    Create basic_library.py

---
+++ 100 - 기초/basic_library.py
@@ -0,0 +1,16 @@
+from itertools import combinations
+from itertools import permutations
+from itertools import product
+from itertools import combinations_with_replacement as cwr
+
+data = ['A','B','C']
+permu = list(permutations(data, 2)) # 모든 순열 일렬로 나열
+comb = list(combinations(data,2)) # 모든 조합 구하기
+
+prod = list(product(data, repeat=2)) # (중복을 포함한) 모든 순열 구하기
+comWrepl = list(cwr(data, 2)) # (중복을 포함한) 모든 조합 구하기
+print(f'data = {data}')
+print(f'permuation = {permu}\n\
+combinations = {comb} \n\
+permuation allowed duplication = {prod} \n\
+combination allowed duplication = {comWrepl} \n')
